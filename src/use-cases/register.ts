c2a015502fd0f25ee175717d6f2adae7f3721e0d
Victor Prado <prado.victor1005@gmail.com>
2024-08-01 23:19:14 -0300

    criando controller e repository de criacao de usuario

---
+++ src/use-cases/register.ts
@@ -0,0 +1,32 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: any) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 8)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists')
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+}
